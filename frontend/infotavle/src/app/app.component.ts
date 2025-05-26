edef7e396df4a06722d5f6590ff922f129a2b1e7
nelbom1234 <79920825+nelbom1234@users.noreply.github.com>
2025-05-26 14:32:18 +0200

    set locale to danish

--- frontend/infotavle/src/app/app.component.ts
+++ frontend/infotavle/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from "./banner/banner.component";
 import { CalendarComponent } from "./calendar/calendar.component";
+import myLocaleDa from '@angular/common/locales/da';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(myLocaleDa);
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, BannerComponent, CalendarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    {provide: LOCALE_ID, useValue: 'da'}
+  ]
 })
 export class AppComponent {
   title = 'infotavle';
