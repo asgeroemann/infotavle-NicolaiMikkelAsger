a54d2761e0f8acf0c6cb610d7266afff22d2e39f
nelbom1234 <79920825+nelbom1234@users.noreply.github.com>
2025-06-13 09:59:40 +0200

    add some changes from the main branch and make them roughly work together

--- frontend/infotavle/src/app/calendar/day/day.component.ts
+++ frontend/infotavle/src/app/calendar/day/day.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject, OnInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventsService } from '../../events.service';
 
 @Component({
   selector: 'app-day',
@@ -7,8 +8,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './day.component.html',
   styleUrl: './day.component.css'
 })
-export class DayComponent {
+export class DayComponent implements OnInit {
   @Input() date!: Date;
-  @Input() holiday!: string;
-  @Input() event!: string;
+
+  holiday : string = "";
+
+  todaysEvents:Array<any> =[];
+
+  eventsService: EventsService = inject(EventsService);
+
+  constructor(){
+    //this.todaysEvents = this.eventsService.getEventsByDate(this.date);
+    //console.log(this.date);
+  }
+
+  ngOnInit(){
+    this.todaysEvents = this.eventsService.getEventsByDate(this.date);
+    console.log(this.date.toISOString());
+    this.holiday = this.eventsService.maerkeDag(this.date);
+  }
+
 }
---
+++ frontend/infotavle/src/app/events.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+// import {Holidays } from '@salling-group/holidays';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  url = 'http://localhost:5115/api/events';
+
+  events: object = {}; // Array<any> = [];
+
+  maerkedage: object = {};
+
+  test = [{"eventID":1,"theDate":"2025-06-04T00:00:00","theMessage":"Idræt","slideID":null},{"eventID":1,"theDate":"2025-06-11T00:00:00","theMessage":"Idræt","slideID":null},{"eventID":2,"theDate":"2025-06-05T00:00:00","theMessage":"Fri (Grundlovsdag)","slideID":null},{"eventID":6,"theDate":"2025-06-06T00:00:00","theMessage":"Fri","slideID":null},{"eventID":3,"theDate":"2025-05-27T00:00:00","theMessage":"På tur","slideID":4},{"eventID":4,"theDate":"2025-05-27T00:00:00","theMessage":"Facility info","slideID":2},{"eventID":4,"theDate":"2025-06-27T00:00:00","theMessage":"Facility info","slideID":2}];
+  testMaerkedage = [{"date":"2025-06-23","name":"Sankt Hans aften","nationalHoliday":false},{"date":"2025-06-24","name":"Sankt Hans dag","nationalHoliday":false}];
+
+  constructor() {
+    //this.events=
+    this.events=/*this.test; */this.fetchEvents();
+    this.maerkedage = this.testMaerkedage//  this.fetchMaerkedage();
+   }
+
+   async fetchEvents() : Promise<object>{
+    const data = await fetch(this.url);
+    //console.log("test");
+    //console.log(data.json());
+    return await data.json() ?? {};
+   }
+
+   async fetchMaerkedage() : Promise<Array<any>>{
+    //Fjern inden commit
+    const token = "Bearer ce6d0fcf-9a9d-4124-8fc5-d6e9c8744b9b";
+    var dato = new Date();
+    dato.setDate(dato.getDate() + 14);
+    const response = await fetch(
+      "https://api.sallinggroup.com/v1/holidays?endDate="
+      + dato.toISOString().slice(0, 10), {
+        headers: {
+        //credentials: "include" ,
+          "Authorization": token,
+        }
+      });
+      const data = await response.json();
+      console.log("teststdrds");
+      console.log(data); 
+    return await data.json() ?? [];
+   }
+
+
+  //  async fetchMaerkedage() : Promise<Array<any>>{ //Variant med Salling-SDK
+  //   //Fjern inden commit
+
+  //   var dato = new Date();
+  //   dato.setDate(dato.getDate() + 14);
+
+  //   const Holidays = require('@salling-group/holidays');
+  //   const instance = new Holidays({
+  //   'applicationName': 'My Application v1.0.0',
+  //   'auth': {
+  //   'type': 'bearer',
+  //   'token': 'my_token',
+  //   },
+  //   });
+
+  //   return instance.holidaysUntil(dato)
+
+  //  }
+
+   getEvents() : object {
+    return this.events;
+   }
+   
+   getEventsByDate(date:Date) : Array<any>  {
+    console.log(this.events);
+    var filter = (this.events as Array<any>).filter(e =>
+      this.dateEqual(date, new Date(e.theDate))
+    )
+    return filter;
+   }
+
+   //Checks whether two dates are equal ignoring hours, minutes etc. 
+   dateEqual(a:Date, b:Date) : boolean {
+    return (a.getFullYear() === b.getFullYear() && a.getMonth() === b.getMonth() && a.getDate() === b.getDate());
+   }
+
+   maerkeDag (date:Date) : string {
+    var filter = (this.maerkedage as Array<any>).filter(e =>
+      this.dateEqual(date, new Date(e.date))
+    )
+    console.log(filter);
+    if (filter.length > 0){ 
+      console.log(filter[0].name);
+      return filter[0].name;
+    }
+    return "";
+   }
+
+}
