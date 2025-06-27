2b8c9e7557277e2cbc06148ed017ca3f2984d9e4
nelbom1234 <79920825+nelbom1234@users.noreply.github.com>
2025-06-27 08:27:32 +0200

    fix testdata

--- sql ny/InfoscreenDatabase_TestData.sql
+++ sql ny/InfoscreenDatabase_TestData.sql
@@ -3,19 +3,19 @@ GO
 
 INSERT INTO slides (componentName, expirationDate, isActive)
 VALUES
-('app-laeringsmaal', GETDATE(), 1),
+('app-laeringsmaal', NULL, 1),
 ('app-weather', NULL, 1),
-('app-rejseplanen', GETDATE(), 1),
+('app-rejseplanen', NULL, 1),
 ('app-feedback', GETDATE(), 0);
 GO
 
 INSERT INTO calendarEvents (theDate, theMessage, slideID)
 VALUES
-('2025-06-25', 'Idræt', NULL),
+('2025-06-25', 'IdrÃ¦t', NULL),
 ('2025-06-27', 'Auditoriet ved kantinen: Gennemgang af Digitale Informationstavler Kl.: 10:00', NULL),
 ('2025-06-30', 'GameJam: Afslut arbejdet.', NULL),
-('2025-07-02', 'Idræt', NULL),
-('2025-07-02', 'GameJam: Fremlæggelse.', NULL),
+('2025-07-02', 'IdrÃ¦t', NULL),
+('2025-07-02', 'GameJam: FremlÃ¦ggelse.', NULL),
 ('2025-07-03', 'Sommerafslutnings arrangement', NULL);
 
 GO
